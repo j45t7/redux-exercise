820c6d714db639e287b8af22c0fe8ed79bb10157
Justyna Marciniak <justy.marciniak@gmail.com>
2021-10-16 19:16:15 +0900

    add note

--- redux-demo.js
+++ redux-demo.js
@@ -4,9 +4,18 @@ const redux = require('redux')
 // two parameters, the old or existing state
 // must return a new state object
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    }
   }
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    }
+  }
+
+  return state
 }
 const store = redux.createStore(counterReducer)
 
@@ -18,3 +27,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber)
 
 store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
